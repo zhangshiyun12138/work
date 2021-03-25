2f3c94ac586ac53ae73309cc62a1899f9f58a0fb
zhangshiyun12138 <1146797510@qq.com>
2021-03-25 11:32:01 +0800

    计算器算法作业

--- calc_homework/conftest.py
+++ calc_homework/conftest.py
@@ -1,19 +1,17 @@
 import pytest
-import allure
 import yaml
+import allure
 from calc_homework.calc_case import Calc
 
-#创建fix方法
-@pytest.fixture(scope="module")  #将fixture作用域设置为module级别
+#创建fixture方法
+@pytest.fixture(scope="module")  #将fixture方法的作用域为module级别
 def get_calc():
-    #执行测试用例前打印【开始计算】
-    print("计算开始")
+    print("开始计算")   #用例执行前打印【开始计算】
     calc = Calc()
     yield calc
-    #执行完测试用例后打印【结束计算】
-    print("结束计算")
+    print("结束计算")   #用例执行完后打印【结束计算】
 
-#健在yaml文件，对里边的值分别赋值给变量
+#获取yaml文件，并对里边的数据赋值给变量，给后续测试方法想要参数化
 with open('calc.yaml') as f:
     datas = yaml.safe_load(f)
     add_data = datas['add_value']
@@ -25,26 +23,22 @@ with open('calc.yaml') as f:
     div_data = datas['div_value']
     div_ids = datas['div_myids']
 
-#获取加法数据，并返回
-@pytest.fixture(params=add_data,ids=add_ids)
+@pytest.fixture(params=add_data,ids=add_ids)  #获取加法数据，并返回
 def get_add_data(request):
     data = request.param
     return data
 
-#获取减法数据。并返回
-@pytest.fixture(params=sub_data,ids=sub_ids)
+@pytest.fixture(params=sub_data,ids=sub_ids)  #获取加法数据，并返回
 def get_sub_data(request):
     data = request.param
     return data
 
-#获取乘法数据，并返回
-@pytest.fixture(params=mul_data,ids=mul_ids)
+@pytest.fixture(params=mul_data,ids=mul_ids)  #获取乘法数据，并返回
 def get_mul_data(request):
     data = request.param
     return data
 
-#获取除法数据，并返回
-@pytest.fixture(params=div_data,ids=div_ids)
+@pytest.fixture(params=div_data,ids=div_ids)  #获取除法数据，并返回
 def get_div_data(request):
     data = request.param
     return data
--- calc_homework/test_calculator.py
+++ calc_homework/test_calculator.py
@@ -3,37 +3,44 @@ import pytest
 import allure
 
 @allure.feature("测试计算器算法")
-#定义算法类
-class TestCalc:
+class TestCalc():  #闯将计算测试类
+
     @allure.story("加法算法测试")
-    @pytest.mark.run(order=1)  #控制加法测试用例顺序第一个执行
-    def test_add(self,get_calc,get_add_data): #定义测试加法用例，实例化加法
+    @pytest.mark.run(order=1)  #控制加法测试用例为顺序第一个
+    def test_add(self,get_calc,get_add_data):  #定义测试加法用例，并实例话加法
         result = get_calc.add(get_add_data[0],get_add_data[1])
-        if isinstance(result,float):  #判断result数据类型，如果是浮点型，result强制保留小数后两位
+        #断言result的数据类型，如果为浮点型，则强制保留小数后两位
+        if isinstance(result,float):
             result = round(result,2)
-        #断言是否相等，相等则通过，不相等则失败
+        #断言结果是否相等，如果相等则通过，如果不相等则失败
         assert result == get_add_data[2]
 
-    @pytest.mark.run(order=3)  #控制乘法测试用例顺序第三个执行
-    def test_mul(self,get_calc,get_mul_data): #定义测试乘法用例，实例化乘法
-        result = get_calc.add(get_mul_data[0],get_mul_data[1])
-        if isinstance(result,float):  #判断result诗句类型，如果是浮点型，result强制保留小数后两位
-            result =round(result,2)
-        #断言结果是否相等，相等则通过，不相等则失败
-        assert result == get_mul_data[2]
-
-    @pytest.mark.run(order=4) #控制除法测试用例顺序第三个执行
-    def test_div(self,get_calc,get_div_data): #定义除法测试用例，实例化除法
+    @allure.story("除法算法")
+    @pytest.mark.run(order=4)  #控制除法测试用例为顺序第四个
+    def test_div(self,get_calc,get_div_data):  #定义除法测试用例，并实例话除法
         result = get_calc.add(get_div_data[0],get_div_data[1])
-        if isinstance(result,float):  #判断result数据类型，如果是浮点型，result强制保留小数后两位
+        #断言result的数据类型，如果为浮点型，则强制保留小数后两位
+        if isinstance(result,float):
             result = round(result,2)
         #断言结果是否相等，相等则通过，不相等则失败
         assert result == get_div_data[2]
 
-    @pytest.mark.run(order=2) #减法测试用例顺序第二个执行
-    def test_sub(self,get_calc,get_sub_data): #定义减法测试用例，实例化减法
+    @allure.story("减法算法")
+    @pytest.mark.run(order=2)  #控制减法测试用例为顺序第四个
+    def test_sub(self,get_calc,get_sub_data):  #定义减法测试用例，并实例话减法
         result = get_calc.add(get_sub_data[0],get_sub_data[1])
-        if isinstance(result,float):  #判断result数据类型，如果是浮点型，result强制保留小数后两位
+        #断言result的数据类型，如果为浮点型，则前置保留小数后两位
+        if isinstance(result,float):
             result = round(result,2)
-        #断言结果是否相等，相等则通过，不相等则失败
+        #断言结果是否相等，如果相等则通过，不相等则失败
         assert result == get_sub_data[2]
+
+    @allure.story("乘法算法")
+    @pytest.mark.run(order=3)  #控制乘法测试用例为顺序第三个
+    def test_mul(self,get_calc,get_mul_data):  #定义乘法测试用例，并实例化乘法
+        result = get_calc.add(get_mul_data[0],get_mul_data[1])
+        #断言result的数据类型，如果为浮点型，则强制保留小数点后两位
+        if isinstance(result,float):
+            result = round(result,2)
+        #断言结果是否相等，如果相等则通过，如果不相等则失败
+        assert result == get_mul_data[2]
